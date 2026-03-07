611b1294881d4ecffd500843fdf2d6bc6bc3532f
Richard Azzambuja <richardbarreeto@gmail.com>
2026-03-07 11:43:26 -0300

    Adicionando list_detail no genero

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from genres.views import genre_view
+from genres.views import genre_create_list_view, genre_detail_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genre/', genre_view, name='genre_view'),
+    path('genre/', genre_create_list_view, name='genre_create_list'),
+    path('genre/<int:pk>/', genre_detail_view, name='detail_list')
 ]
--- genres/views.py
+++ genres/views.py
@@ -2,10 +2,11 @@ from django.http import JsonResponse
 from genres.models import Genre
 import json
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 
 #Da forma com compressao
 @csrf_exempt
-def genre_view(request):
+def genre_create_list_view(request):
     if request.method == 'GET':
         genres = Genre.objects.all()
         data = [{'id': genre.id, 'name': genre.name}for genre in genres]
@@ -18,3 +19,10 @@ def genre_view(request):
         return JsonResponse(
             {'id': new_genre.id, 'name': new_genre.name},
             status=201)
+    
+
+@csrf_exempt
+def genre_detail_view(request, pk):
+    genre = get_object_or_404(Genre, pk=pk)
+    data = {"id": genre.id, "name": genre.name}
+    return JsonResponse(data)
